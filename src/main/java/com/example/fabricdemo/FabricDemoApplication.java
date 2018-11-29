ae852d22a416818cd64ad3f019e79b67a061196c
unknown <gaoyp@yahoo.com>
2018-11-29 18:21:27 +0800

    init

---
+++ src/main/java/com/example/fabricdemo/FabricDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.fabricdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricDemoApplication.class, args);
+	}
+}
